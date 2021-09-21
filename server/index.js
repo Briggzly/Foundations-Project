d10988fe5d4feaf6d03ce9bb7afbedd14d71a800
Kyson Briggs <kysonjbriggs@gmail.com>
2021-09-21 08:02:43 -0600

    added deployment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
 
-SERVER_PORT = 5500;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Server running on port ${SERVER_PORT}`)
+
+const port = process.env.PORT || 4007;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
